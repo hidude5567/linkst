258c67ca92e7426ae707e479aa7a3a59449a190a
hidude5567 <nolansenter@s.fwc.org>
2026-01-16 11:57:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function go() {
-  location.href = "capca.html"
-  
+function openSite(url) {
+  var pswd = prompt("password")
+    if (pswd === "1234") {
+      var win = window.open('about:blank', '_blank');
+      var iframe = win.document.createElement('iframe');
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url;
+      win.document.body.appendChild(iframe);
+    }
 }
